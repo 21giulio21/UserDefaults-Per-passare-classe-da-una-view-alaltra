03aaa31f1a4b34930b53785439a162296166a84b
Giulio Tavella <giulioTavella@MacBook-Pro-di-Giulio.local>
2017-01-30 21:06:34 +0100

    Initial Commit

---
+++ UserDefaults Per passare classe da una view alaltra/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UserDefaults Per passare classe da una view alaltra
+//
+//  Created by Giulio Tavella on 30/01/17.
+//  Copyright © 2017 Giulio Tavella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
